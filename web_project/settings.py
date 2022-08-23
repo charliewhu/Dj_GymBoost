0ae1df950b70845bed8af9d90097d77626097680
Charlie <charlieavery123@hotmail.co.uk>
2022-08-23 09:52:41 +0100

    moved secret key and debug to .env

--- web_project/settings.py
+++ web_project/settings.py
@@ -25,10 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-8$2d=hl-i+wdrstga$^emqrt+@24ol^rl1xn9$nj(cnalx)&o^"
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG", False)
 
 ALLOWED_HOSTS = []
 
