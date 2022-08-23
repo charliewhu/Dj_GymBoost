8bbfa2b5305b5f86a166bc7288b0bb89c87e1b8b
Charlie <charlieavery123@hotmail.co.uk>
2022-08-23 08:35:33 +0100

    created custom user model

--- web_project/settings.py
+++ web_project/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     "accounts",
 ]
 
-# AUTH_USER_MODEL = "accounts.User"
+AUTH_USER_MODEL = "accounts.User"
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
