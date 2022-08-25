5920539b36c645d4fc8f2fd5a01bbbe1b9b42c33
Charlie <charlieavery123@hotmail.co.uk>
2022-08-25 13:05:30 +0100

    passing unit test

--- workouts/tests.py
+++ workouts/tests.py
@@ -1,9 +1,9 @@
 from django.test import TestCase
 from django.urls import resolve
-from .views import home_page
+from .views import home
 
 # Create your tests here.
 class HomePageTest(TestCase):
-    def test_root_url_resolves_to_home_page_view(self):
+    def test_root_url_resolves_to_home_view(self):
         found = resolve("/")
-        self.assertEqual(found.func, home_page)
+        self.assertEqual(found.func, home)
