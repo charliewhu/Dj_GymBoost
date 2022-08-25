24b54394ae3d89d7851834de30d611eedfafdbdf
Charlie <charlieavery123@hotmail.co.uk>
2022-08-25 16:56:13 +0100

    passing func test and created helpers.py

--- features/steps/create_workout.py
+++ features/steps/create_workout.py
@@ -1,6 +1,8 @@
 from behave import given, when, then
 from selenium.webdriver.common.by import By
 
+from features.helpers import get_inner_html
+
 
 @given("I am on the homepage")
 def step_impl(context):
@@ -10,18 +12,14 @@ def step_impl(context):
 
 @when("I click on the Create Workout button")
 def step_impl(context):
-    create_workout_btn = context.browser.find_element(By.ID, "create_workout_btn")
     context.test.assertIn(
-        "Create Workout", create_workout_btn.get_attribute("innerHTML")
+        "Create Workout", get_inner_html(context, "create_workout_btn")
     )
+    create_workout_btn = context.browser.find_element(By.ID, "create_workout_btn")
     create_workout_btn.click()
-
-
-@then("I will create a Workout")
-def step_impl(context):
-    raise NotImplementedError()
 
 
 @then("I will see the Workout screen for that Workout")
 def step_impl(context):
-    raise NotImplementedError()
+    context.test.assertIn("Workout", get_inner_html(context, "title"))
+    list = context.browser.find_element(By.ID, "workout_exercise_list")
