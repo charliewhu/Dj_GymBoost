c810e151a5dd7f301be27e7c909032fe94b5b0e0
Charlie <charlieavery123@hotmail.co.uk>
2022-08-25 14:13:16 +0100

    passing first functional test

--- workouts/tests.py
+++ workouts/tests.py
@@ -1,5 +1,7 @@
 from django.test import TestCase
 from django.urls import resolve
+from django.http import HttpRequest
+
 from .views import home
 
 # Create your tests here.
@@ -7,3 +9,11 @@ class HomePageTest(TestCase):
     def test_root_url_resolves_to_home_view(self):
         found = resolve("/")
         self.assertEqual(found.func, home)
+
+    def test_home_page_returns_correct_html(self):
+        request = HttpRequest()
+        reponse = home(request)
+        html = reponse.content.decode("utf8")
+        self.assertTrue(html.startswith("<html>"))
+        self.assertIn("<title>GymBoost Home</title>", html)
+        self.assertTrue(html.endswith("</html>"))
--- workouts/views.py
+++ workouts/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
-def home():
-    pass
+def home(request):
+    return HttpResponse("<html><title>GymBoost Home</title></html>")
