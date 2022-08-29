01dd6f24a9f3bb2d36c6ef99699d30d8ebfcf3e6
Charlie <charlieavery123@hotmail.co.uk>
2022-08-29 14:48:37 +0100

    passing up to unimplemented feature step

--- features/steps/create_exercise.py
+++ features/steps/create_exercise.py
@@ -4,12 +4,12 @@ from selenium.webdriver.common.by import By
 
 @given("I am on the Exercise page")
 def step_impl(context):
-    context.get_url("exercises")
+    context.browser.get(context.get_url("exercises"))
 
 
 @when('I fill the form with "{my_new_exercise}"')
 def step_impl(context, my_new_exercise):
-    context.browser.find_element(By.ID, "exercise_name").send_keys(my_new_exercise)
+    context.browser.find_element(By.ID, "id_name").send_keys(my_new_exercise)
 
 
 @when('click "Create Exercise"')
