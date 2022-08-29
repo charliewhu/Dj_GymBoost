5a25e559c198167ad1420a2e26d90e25181bfd82
Charlie <charlieavery123@hotmail.co.uk>
2022-08-29 14:41:10 +0100

    passed unit tests up to then clause

--- exercises/views.py
+++ exercises/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
-
-from exercises.models import Exercise
+from .forms import ExerciseForm
+from .models import Exercise
 
 # Create your views here.
 def exercises(request):
     workout_id = request.GET.get("workout_id")
-    context = {"exercises": Exercise.objects.all(), "workout_id": workout_id}
+    form = ExerciseForm()
+    context = {
+        "exercises": Exercise.objects.all(),
+        "workout_id": workout_id,
+        "form": form,
+    }
     return render(request, "exercises/exercises.html", context)
