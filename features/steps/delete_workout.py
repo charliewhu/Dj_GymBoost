e5816592d67e00b17d58667def80f6ec3a9c36ee
Charlie <charlieavery123@hotmail.co.uk>
2022-08-29 07:17:45 +0100

    finished writing func tests

--- features/steps/delete_workout.py
+++ features/steps/delete_workout.py
@@ -9,9 +9,10 @@ def step_impl(context):
 
 @when('I click on the "Delete Workout" button')
 def step_impl(context):
-    raise NotImplementedError('STEP: When I click on the "Delete Workout" button')
+    context.browser.find_element(By.ID, "delete_workout_exercise_btn").click()
 
 
 @then("the Workout will not show on the page")
 def step_impl(context):
-    raise NotImplementedError("STEP: Then the Workout will not show on the page")
+    workout_list_items = context.browser.find_elements(By.ID, "workout_list_item")
+    context.test.assertEqual(workout_list_items, [])
