6343d25293f9d2b08269afe23b040722e3484965
Charlie <charlieavery123@hotmail.co.uk>
2022-08-29 12:41:09 +0100

    added to func test and added placeholder html

---
+++ features/steps/create_exercise.py
@@ -0,0 +1,22 @@
+from behave import given, when, then
+from selenium.webdriver.common.by import By
+
+
+@given("I am on the Exercise page")
+def step_impl(context):
+    context.get_url("exercises")
+
+
+@when('I fill the form with "{my_new_exercise}"')
+def step_impl(context, my_new_exercise):
+    context.browser.find_element(By.ID, "exercise_name").send_keys(my_new_exercise)
+
+
+@when('click "Create Exercise"')
+def step_impl(context):
+    context.browser.find_element(By.ID, "exercise_submit_btn")
+
+
+@then('"My New Exercise" will show on the list')
+def step_impl(context):
+    raise NotImplementedError('STEP: Then "My New Exercise" will show on the list')
