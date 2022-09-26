3f0ad2b54c125090dc13369b9d8446781a757825
Charlie <charlieavery123@hotmail.co.uk>
2022-09-26 14:08:11 +0100

    removed tests to only test custom queryset logic

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -8,9 +8,9 @@ from exercises.tests.factory import ExerciseFactory
 class ExerciseTest(APITestCase):
     def setUp(self):
         self.url = "/api/exercises/"
+        self.exercise = ExerciseFactory()
 
     def test_GET(self):
-        self.exercise = ExerciseFactory()
         res = self.client.get(self.url)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res["content-type"], "application/json")
@@ -20,44 +20,3 @@ class ExerciseTest(APITestCase):
                 {"id": self.exercise.id, "name": self.exercise.name},
             ],
         )
-
-    def test_POSTing_a_new_item(self):
-        exercise_name = "new exercise"
-        response = self.client.post(
-            self.url,
-            {"name": exercise_name},
-        )
-        exercise = Exercise.objects.first()
-
-        self.assertEqual(response.status_code, 201)
-        self.assertEqual(exercise.name, exercise_name)
-
-    def test_GET_single_object(self):
-        self.exercise = ExerciseFactory()
-        res = self.client.get(self.url + "1/")
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(res["content-type"], "application/json")
-        self.assertEqual(
-            json.loads(res.content.decode("utf8")),
-            {"id": self.exercise.id, "name": self.exercise.name},
-        )
-
-    def test_PUT_object(self):
-        self.exercise = ExerciseFactory()
-        new_name = "newName"
-        res = self.client.put(
-            self.url + "1/",
-            {"name": new_name},
-        )
-        exercise = Exercise.objects.first()
-
-        self.assertEqual(res.status_code, 200)
-        self.assertEqual(exercise.name, new_name)
-
-    def test_DELETE_object(self):
-        self.exercise = ExerciseFactory()
-        res = self.client.delete(self.url + "1/")
-        exercises = Exercise.objects.count()
-
-        self.assertEqual(res.status_code, 204)
-        self.assertEqual(exercises, 0)
