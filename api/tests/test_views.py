ce0b675782279d5d4665fdeff9bfd801640e7510
Charlie <charlieavery123@hotmail.co.uk>
2022-09-26 13:34:16 +0100

    added get exercises unit test

--- api/tests/test_views.py
+++ api/tests/test_views.py
@@ -1,15 +1,17 @@
 import json
 from django.test import TestCase
 
+from exercises.models import Exercise
 from exercises.tests.factory import ExerciseFactory
 
 
 class ExerciseTest(TestCase):
     def setUp(self):
-        self.exercise = ExerciseFactory()
+        self.url = "/api/exercises/"
 
-    def test_get_returns_json_200(self):
-        res = self.client.get("/api/exercises/")
+    def test_GET(self):
+        self.exercise = ExerciseFactory()
+        res = self.client.get(self.url)
         self.assertEqual(res.status_code, 200)
         self.assertEqual(res["content-type"], "application/json")
         self.assertEqual(
