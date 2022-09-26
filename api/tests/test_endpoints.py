b35a2cdf8a00d0ce7e387cc23d64dcd96494ea5d
Charlie <charlieavery123@hotmail.co.uk>
2022-09-26 17:25:06 +0100

    wrote routines test

--- api/tests/test_endpoints.py
+++ api/tests/test_endpoints.py
@@ -1,9 +1,8 @@
 import json
 from rest_framework.test import APITestCase
 
-from exercises.models import Exercise
 from exercises.tests.factory import ExerciseFactory
-
+from routines.tests.factory import RoutineFactory
 
 class ExerciseTest(APITestCase):
     def setUp(self):
@@ -18,5 +17,21 @@ class ExerciseTest(APITestCase):
             json.loads(res.content.decode("utf8")),
             [
                 {"id": self.exercise.id, "name": self.exercise.name},
+            ],
+        )
+
+class RoutineTest(APITestCase):
+    def setUp(self):
+        self.url = "/api/routines/"
+        self.routine = RoutineFactory()
+
+    def test_GET(self):
+        res = self.client.get(self.url)
+        self.assertEqual(res.status_code, 200)
+        self.assertEqual(res["content-type"], "application/json")
+        self.assertEqual(
+            json.loads(res.content.decode("utf8")),
+            [
+                {"id": self.routine.id, "name": self.routine.name},
             ],
         )
